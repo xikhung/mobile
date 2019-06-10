9f849775743d5bfcf988fbeada6493370adad7cb
Alexander Hung <ahung@xik.gt>
2019-06-10 11:15:12 -0600

    Agregando Assert y Report para pruebas del reporte maven surefire

--- src/test/java/Test/PruebaAndroidLocal.java
+++ src/test/java/Test/PruebaAndroidLocal.java
@@ -1,6 +1,8 @@
 package Test;
 
 import Auxiliar.TestBase;
+import org.testng.Assert;
+import org.testng.Reporter;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
@@ -9,5 +11,7 @@ public class PruebaAndroidLocal extends TestBase {
     @Test(description = "Prueba local de driver")
     public void openApp(String platformName, String deviceName, String udid, String appPackage, String appActivity){
         System.out.println("Prueba local de driver");
+        Reporter.log("Prueba local de driver");
+        Assert.assertTrue(true);
     }
 }
