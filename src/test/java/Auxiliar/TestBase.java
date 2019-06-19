e08ea11a83bd6f55f28b9e8ef1f8d9b00932a06f
Alexander Hung <ahung@xik.gt>
2019-06-19 13:18:35 -0600

    Agregando el listener

--- src/test/java/Auxiliar/TestBase.java
+++ src/test/java/Auxiliar/TestBase.java
@@ -4,7 +4,9 @@ import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 
+@Listeners(Report.CustomListener.class)
 public class TestBase extends GeneralMethods {
     private AppiumDriver driver;
     private AppiumDriverLocalService server;
