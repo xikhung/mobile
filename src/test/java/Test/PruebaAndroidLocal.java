be6a2b10558a455f0f4ea00150a5e245dcbccaca
Alexander Hung <ahung@xik.gt>
2019-06-19 13:20:45 -0600

    Test de prueba de login para app WordPress

--- src/test/java/Test/PruebaAndroidLocal.java
+++ src/test/java/Test/PruebaAndroidLocal.java
@@ -1,17 +1,19 @@
 package Test;
 
 import Auxiliar.TestBase;
-import org.testng.Assert;
-import org.testng.Reporter;
+import POMs.WordPress.Login;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+import Report.ExtentTestManager;
+
 
 public class PruebaAndroidLocal extends TestBase {
+
     @Parameters({"platformName","deviceName","UDID","appPackage","appActivity"})
     @Test(description = "Prueba local de driver")
     public void openApp(String platformName, String deviceName, String udid, String appPackage, String appActivity){
-        System.out.println("Prueba local de driver");
-        Reporter.log("Prueba local de driver");
-        Assert.assertTrue(true);
+        ExtentTestManager.startTest("OpenApp", "Prueba local de driver");
+        Login androidLogin = new Login(getDriver());
+        androidLogin.tapLogIn();
     }
 }
