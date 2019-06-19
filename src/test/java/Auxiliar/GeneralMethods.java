672b4de7f080de60ab0f8b68fc9ca06f2a81f1f0
Alexander Hung <ahung@xik.gt>
2019-06-19 13:17:25 -0600

    Agregando el metodo nativo de tap

--- src/test/java/Auxiliar/GeneralMethods.java
+++ src/test/java/Auxiliar/GeneralMethods.java
@@ -1,8 +1,15 @@
 package Auxiliar;
 
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.touch.TapOptions;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.ElementOption;
+
+import java.time.Duration;
 
 public class GeneralMethods {
 
@@ -10,12 +17,19 @@ public class GeneralMethods {
         return driver instanceof AndroidDriver;
     }
 
-    protected void waitms(int milliseconds){
+
+    protected void waitMS(int milliseconds){
         try{
             wait(milliseconds);
         }catch(InterruptedException error){
             error.printStackTrace();
         }
+    }
+
+    protected void singleTap(AppiumDriver driver, MobileElement element){
+        new TouchAction(driver)
+                .tap(TapOptions.tapOptions().withElement(ElementOption.element(element)))
+                .waitAction(WaitOptions.waitOptions(Duration.ofMillis(250))).perform();
     }
 
     protected void closeDriver(AppiumDriver driver){
